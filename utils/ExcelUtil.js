af7e28e4b7135b355ace5da4b2126a2f59d758a0
MohitAman <mohit_a@pursuitsoftware.com>
2026-09-23 19:00:45 +0530

    assignment done 23-09-2026

--- utils/ExcelUtil.js
+++ utils/ExcelUtil.js
@@ -4,25 +4,14 @@ const path = require('path');
 class ExcelUtil {
 
     constructor() {
-        this.filePath = path.join(
-            process.cwd(),
-            'testdata',
-            'TestData.xlsx'
-        );
+        this.filePath = path.join(process.cwd(),'testdata','TestData.xlsx');
     }
 
     getTestData(sheetName, testCase) {
-
         const workbook = XLSX.readFile(this.filePath);
-
         const worksheet = workbook.Sheets[sheetName];
-
         const data = XLSX.utils.sheet_to_json(worksheet);
-
-        const testData = data.filter(
-            row => row.TestCase === testCase
-        );
-
+        const testData = data.filter(row => row.TestCase===testCase);
         return testData;
     }
 }
