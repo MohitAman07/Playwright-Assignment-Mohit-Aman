1b60091cc3c750b52ecfecf691b8a63195b8bc38
MohitAman <mohit_a@pursuitsoftware.com>
2026-09-23 18:54:33 +0530

    Fix Excel utility path import

--- utils/ExcelUtil.js
+++ utils/ExcelUtil.js
@@ -1,24 +1,28 @@
-import XLSX from 'xlsx';
-import path from 'path';
-
 const XLSX = require('xlsx');
+const path = require('path');
 
 class ExcelUtil {
 
-     constructor() {
-        this.filePath = path.join(process.cwd(),'testdata','TestData.xlsx');
+    constructor() {
+        this.filePath = path.join(
+            process.cwd(),
+            'testdata',
+            'TestData.xlsx'
+        );
     }
 
-    getTestData(sheetName,testCase) 
-    {
-        const workbook=XLSX.readFile(this.filePath);
-        const worksheet=workbook.Sheets[sheetName];
-        const data=XLSX.utils.sheet_to_json(worksheet);
-        const testData=data.filter(row=>row.TestCase===testCase);
-        if (testData.length===0) 
-            {
-                throw new Error(`Test data not found for Test Case: ${testCase}`);
-            }
+    getTestData(sheetName, testCase) {
+
+        const workbook = XLSX.readFile(this.filePath);
+
+        const worksheet = workbook.Sheets[sheetName];
+
+        const data = XLSX.utils.sheet_to_json(worksheet);
+
+        const testData = data.filter(
+            row => row.TestCase === testCase
+        );
+
         return testData;
     }
 }
